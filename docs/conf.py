f96beef7d27f96d6ef6d3a2f647e36444afdb20e
Cody Logan <clpo13@gmail.com>
2020-01-08 10:55:28 -0800

    Update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,12 @@ release = '0.2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
+    'm2r',
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,13 +51,17 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
 }
 
+napoleon_google_docstring = True
+napoleon_use_param = True
+napoleon_use_ivar = True
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -6,6 +6,8 @@
 Welcome to alphanum's documentation!
 ====================================
 
+.. mdinclude:: ../README.md
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
